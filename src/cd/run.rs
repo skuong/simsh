a5c2af8211ebd468b4d3bc952c7cd85491b1f6d9
Sokhuong Uon <sokhuong.usk@gmail.com>
2026-06-04 22:06:35 +0700

    feat: can navigate to home dir using cd ~ [skip ci]

--- src/cd/run.rs
+++ src/cd/run.rs
@@ -1,6 +1,19 @@
 use std::{env, path::Path};
 pub fn run(dir: &str) {
+    if dir == "~"
+        && let Ok(home) = env::var("HOME")
+    {
+        let home_path = Path::new(&home);
+
+        if env::set_current_dir(home_path).is_err() {
+            println!("Can't go home");
+        }
+
+        return;
+    }
+
     let dir_path = Path::new(dir);
+
     if env::set_current_dir(dir_path).is_err() {
         println!("cd: {}: No such file or directory", dir);
     }
