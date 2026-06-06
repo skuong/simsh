ab0bc7639994c471209a02a1c61412534629ba51
Sokhuong Uon <sokhuong.usk@gmail.com>
2026-06-06 13:57:45 +0700

    feat: handle \ escape character outside of quotes [skip ci]

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,32 +2,58 @@ pub(crate) fn command_input_parser(input: &str) -> Vec<String> {
     let mut args = Vec::new();
     let mut current_arg = String::new();
     let mut last_quote = None;
+    let mut last_slash = None;
 
     for character in input.chars() {
-        match (character, last_quote) {
-            ('"', None) => {
-                last_quote = Some('"');
+        match (character, last_quote, last_slash) {
+            ('\\', None, None) => {
+                last_slash = Some('\\');
             }
-            ('"', Some('"')) => {
-                last_quote = None;
-            }
-            ('\'', Some('"')) => {
+            ('\\', None, Some('\\')) => {
+                last_slash = None;
                 current_arg.push(character);
             }
-            ('\'', None) => {
-                last_quote = Some('\'');
+
+            ('"', None, slash) => {
+                if let Some(_) = slash {
+                    current_arg.push(character);
+                    last_slash = None;
+                    continue;
+                }
+                last_quote = Some('"');
             }
-            ('\'', Some('\'')) => {
+            ('"', Some('"'), _) => {
                 last_quote = None;
             }
-            (char, None) if char.is_whitespace() => {
+            ('\'', Some('"'), _) => {
+                current_arg.push(character);
+            }
+            ('\'', None, slash) => {
+                if let Some(_) = slash {
+                    current_arg.push(character);
+                    last_slash = None;
+                    continue;
+                }
+                last_quote = Some('\'');
+            }
+            ('\'', Some('\''), _) => {
+                last_quote = None;
+            }
+            (char, None, slash) if char.is_whitespace() => {
+                if let Some(_) = slash {
+                    current_arg.push(character);
+                    last_slash = None;
+                    continue;
+                }
+
                 if !current_arg.is_empty() {
                     args.push(current_arg);
                     current_arg = String::new();
                 }
             }
-            (char, _) => {
+            (char, _, _) => {
                 current_arg.push(char);
+                last_slash = None;
             }
         }
     }
